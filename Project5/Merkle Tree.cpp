b911937dfd6fda1bf334be1a0ee95cfed3c534e8
YIKEgroup10num2 <129512661+YIKEgroup10num2@users.noreply.github.com>
2023-07-30 16:42:39 +0800

    Update Merkle Tree.cpp

--- Project5/Merkle Tree.cpp
+++ Project5/Merkle Tree.cpp
@@ -1,76 +1,102 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include<iostream>
 using namespace std;
 
 typedef unsigned int uint;
 
-// Merkle Tree 结构体定义 
+// Merkle Tree 
 typedef struct MerkleTreeNode {
-	struct MerkleTreeNode* left;		
-	struct MerkleTreeNode* right;		
-	struct MerkleTreeNode* parent;		
-	uint level;							// 当前树深，底层为0 
-	uint data;							// 当前节点的值，最底层为原始数据，其上均为计算的哈希值 
+	struct MerkleTreeNode* left;
+	struct MerkleTreeNode* right;
+	struct MerkleTreeNode* parent;
+	uint hash_num;		// 哈希值 ，如果创建树过程中，该值为当前节点的树深度 
+	uint data;
 }MerkleTree;
 
-// 定义宏函数，创建一个 Merkle Tree节点
 #define New_Merkle_Node(mt, tree_depth) {	\
 	mt = (MerkleTree *)malloc(sizeof(MerkleTree)); \
 	mt->left = NULL; \
 	mt->right = NULL; \
 	mt->parent = NULL; \
-	mt->level = (uint)tree_depth; \
+	mt->hash_num = (uint)tree_depth; \
 	mt->data = 0;	\
 	}
 
-// 打印 Merkle tree，中续遍历方式 
+// 打印 Merkle tree 
+int first = 0;
 void Print_Merkle_Tree(MerkleTree* mt, int high)
 {
 	MerkleTree* p = mt;
 	int i;
 
-	if (p == NULL)
+	if (p == NULL) {
 		return;
-
+	}
 	if (p->left == NULL && p->right == NULL) {
-		for (i = 0; i < high - p->level; i++)
-			printf("           ");
+		printf("\n");
+		for (i = 0; i < high - p->hash_num; i++)
+			printf("      ");
 
-		printf("------>%s\n", p->data);
+		printf("--->%2d\n", p->data);
+		first = 1;
+
+		return;
 	}
 	else {
 		Print_Merkle_Tree(mt->left, high);
-		printf("\n");
-		for (i = 0; i < high - p->level; i++)
-			printf("           ");
 
-		printf("------>%-6d\n", p->data);
+		if (first == 1) {
+			for (i = 0; i < high - p->hash_num; i++)
+				printf("      ");
+
+			printf("--->");
+		}
+		else
+			printf("--->");
+
+		printf("%2d", p->hash_num);
+		first = 0;
 
 		Print_Merkle_Tree(mt->right, high);
+		//printf("\n");
 	}
 }
 
+// 计算一个字符串的hash值 
+uint hash_string(char* key) {
+	uint cal = 11, hash = 0;
+	while (*key != '\0' && *key != 0) {
+		hash = hash * cal + *key;
+		key++;
+	}
+	return hash & 0x7FFFFFFF;
+}
+
+// 计算一个字符串的hash值 
+uint hash_int(int num) {
+	uint cal = 11, hash = 0;
+
+	hash = num * cal;
+	return hash & 0x7FFFFFFF;
+}
 
 // 计算两个整数的hash 值 
-uint hash_two_node(uint num1, uint num2) {
+uint hash_uint(uint num1, uint num2) {
 	uint cal = 9, hash = 0;
 	hash = num1 + num2;
 	hash *= cal;
 	return hash & 0x7FFFFFFF;
 }
 
-// 遍历二叉树，找到最后一个叶子节点，遍历方式为前续遍历方式 
+// 遍历二叉树，如果找到空的叶子节点返回叶子结点指针
+// 如果没有找到，即为满二叉树，则返回 NULL 
 MerkleTree* Find_Last_Node(MerkleTree* mt) {
 	MerkleTree* p = mt, * tmp;
 
-	// 如果左右子节点都是NULL，肯定是叶子节点，直接返回 
-	if (p->left == NULL && p->right == NULL)
+	if (p->left == NULL && p->right == NULL)	// 叶子节点 
 		return p;
-	// 如果 右子节点为空，左子节点不为空，继续遍历左子节点，直到叶子节点 
 	else if (p->right == NULL && p->left != NULL)
 		return Find_Last_Node(p->left);
-	// 如果 右子节点不为空，遍历右子节点 
 	else if (p->right != NULL)
 		return Find_Last_Node(p->right);
 }
@@ -79,152 +105,151 @@ MerkleTree* Find_Last_Node(MerkleTree* mt) {
 MerkleTree* Find_Empty_Node(MerkleTree* mt) {
 	MerkleTree* p = mt->parent;
 
-	// 向上找到第一个存在空缺的节点，直到顶节点 
-	while (p->left != NULL && p->right != NULL && p->parent != NULL)  p = p->parent;
-
-	// 如果 p 指向顶节点，说明当前是满二叉树 ，返回 NULL 
-	if (p->parent == NULL && p->left != NULL && p->right != NULL)
+	while (p->left != NULL && p->right != NULL && p->parent != NULL) {
+		p = p->parent;
+	}
+	if (p->parent == NULL && p->left != NULL && p->right != NULL) {		// 到顶了
+		//printf("当前节点位置，p->hash_num=%d, 到顶了！！！ \n", p->hash_num); 
 		return NULL;
-	// 否则当前节点就是要插入节点的位置 
-	else
+	}
+	else {
+		//printf("当前节点位置，p->hash_num=%d \n", p->hash_num); 
 		return p;
+	}
+}
+
+// 更新树的哈希值 
+void update_hash_Merkle(MerkleTree* mt, int tree_depth)
+{
+	if (mt->hash_num == 0) {
+		mt->hash_num = 0;
+	}
+	else if (mt->hash_num == 1) {
+		mt->hash_num = hash_uint(hash_int(mt->left->data), hash_int(mt->right->data));
+	}
+	else {
+		update_hash_Merkle(mt->left, tree_depth - 1);
+		update_hash_Merkle(mt->right, tree_depth - 1);
+		mt->hash_num = hash_uint(mt->left->hash_num, mt->right->hash_num);
+	}
 }
 
 // Merkle tree 初始化 (递归实现)
-MerkleTree* Creat_Merkle_Tree(MerkleTree* mt, int* arr, int nums)
+MerkleTree* Creat_Merkle_Tree(MerkleTree* mt, int* arr, int nums, int tree_depth)
 {
 	MerkleTree* node, * tmp, * p;
 	int i;
-
 	if (nums == 0) {
+		update_hash_Merkle(mt, tree_depth);
+		printf("创建完毕\n");
 
-		printf("Merkle Tree 创建完毕！！！\n");
+		if (mt != NULL) {
+			first = 0;
+			printf("\n开始打印当前 Merkle 树:\n");
+			Print_Merkle_Tree(mt, mt->hash_num);
+			printf("\n");
+		}
 		return mt;
 	}
 	else {
+		printf("叶子节点 [%d] arr=%d, nums=%d, tree_depth=%d\n", __LINE__, *arr, nums, tree_depth);
+		// 每次添加一个叶子节点，优先左树，其次右树
+		// 创建一个树结点 
 		New_Merkle_Node(node, 0);
 		node->data = *arr;
-		printf("新建叶节点 [%d] tree_depth=%d, level=%d, data=%-6d, nums=%d, \n", __LINE__, mt == NULL ? 0 : mt->level, node->level, node->data, nums);
 
-		// 如果 mt 为空，说明当前没有树，需要新建一个头结点 
+		// 如果 mt 为空，说明当前没有树	
 		if (mt == NULL) {
 			New_Merkle_Node(mt, 1);
-			mt->left = node;
+			mt->left = node; 	
 			node->parent = mt;
-			printf("新建头节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, mt->level, mt->data);
-			mt->data = hash_two_node(mt->left->data, mt->right == NULL ? 0 : p->right->data);
-			mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1);
+			tree_depth++;
+			printf("新头结点 [%d] tree_depth=%d, mt->hash_num=%d\n", __LINE__, tree_depth, mt->hash_num);
+			mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1, tree_depth);
 		}
-		//如果 mt 不为空，说明当前已经有树了 
+		// 如果 mt 不为空,mt为头结点 
 		else
 		{
-			//遍历当前树，找到一个空的叶子节点，满二叉树时返回NULL 
-			p = Find_Empty_Node(Find_Last_Node(mt));
+			p = Find_Empty_Node(Find_Last_Node(mt));	// 遍历当前树，找到一个空的叶子节点，满二叉树时返回NULL 
 
-			//如果返回值不为 NULL, 说明已经找到需要插入的节点 
+			// 如果flag 为1 说明存在空的 右叶子节点 
 			if (p != NULL) {
-				if (p->left->left == NULL && p->right == NULL)
+				// 如果最底下就是叶子节点，就直接赋值 
+				if (p->left->hash_num == 0 && p->right == NULL)
 				{
 					p->right = node;
 					node->parent = p;
-					// 更新哈希值 
-					p->data = hash_two_node(p->left->data, p->right == NULL ? 0 : p->right->data);
 				}
-				//如果返回的切点是中间节点 
 				else
-				{     
-					i = p->level - 1;
+				{
+					i = p->hash_num - 1;
 					New_Merkle_Node(tmp, i);
 					p->right = tmp;
 					tmp->parent = p;
-					printf("新中间节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, tmp->level, tmp->data);
 
 					p = p->right;
-					i = p->level - 1;		
+					printf("插入结点 [%d] tree_depth=%d, hash_num=%d\n", __LINE__, tree_depth, p->hash_num);
+
+					i--;
+					// 根据树的深度创建同样深度的左树 
 					while (i > 0) {
-						// 创建中间节点 
 						New_Merkle_Node(tmp, i);
 						p->left = tmp;
 						tmp->parent = p;
-						printf("新中间节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, tmp->level, tmp->data);
 
 						p = p->left;
 						i--;
 					}
-
 					p->left = node;
 					node->parent = p;
-
-					while (p != mt) {
-						p->data = hash_two_node(p->left->data, p->right == NULL ? 0 : p->right->data);
-						p = p->parent;
-					}
-					p->data = hash_two_node(p->left->data, p->right == NULL ? 0 : p->right->data);
-
 				}
-				mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1);
+				mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1, tree_depth);
 			}
-			//如果没有空的节点，说明当前是满二叉树，需要新增头节点了，level也要加1 
+			//如果没有空的叶子节点，则新建一个头结点 
 			else
 			{
-				tmp = mt;				
- 
-				New_Merkle_Node(mt, tmp->level + 1);
-				mt->left = tmp; 		
+				tmp = mt;	
+				tree_depth++; 		
+
+				// 创建一个新的头结点
+				New_Merkle_Node(mt, tree_depth);
+				mt->left = tmp; 	
 				tmp->parent = mt;
-				printf("新建头节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, mt->level, mt->data);
-				New_Merkle_Node(tmp, mt->level - 1);
+				i = tree_depth - 1;	
+				New_Merkle_Node(tmp, i);
 				mt->right = tmp;
 				tmp->parent = mt;
-				printf("新中间节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, tmp->level, tmp->data);
 
+				i--;
 				p = mt->right;
-				i = p->level - 1;
+				printf("插入结点 [%d] tree_depth=%d, hash_num=%d\n", __LINE__, tree_depth, p->hash_num);
 
 				// 根据树的深度创建同样深度的左树 
 				while (i > 0) {
 					New_Merkle_Node(tmp, i);
 					p->left = tmp;
 					tmp->parent = p;
-					printf("新中间节点 [%d] tree_depth=%d, level=%d, data=%-6d \n", __LINE__, mt->level, tmp->level, tmp->data);
 
 					p = p->left;
 					i--;
 				}
+				
 				p->left = node;
 				node->parent = p;
-
-				// 自底向上更新哈希值
-				while (p != mt) {
-					p->data = hash_two_node(p->left->data, p->right == NULL ? 0 : p->right->data);
-					p = p->parent;
-				}
-				// 更新父节点哈希值  
-				p->data = hash_two_node(p->left->data, p->right == NULL ? 0 : p->right->data);
-
-				// 递归调用 
-				mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1);
+				mt = Creat_Merkle_Tree(mt, arr + 1, nums - 1, tree_depth);
 			}
 		}
 	}
 }
 
-
 int main(void)
 {
-	int array[] = {4, 13, 27 ,36 ,47 ,59, 61, 74, 82, 101, 131, 356};
+	// , This Is Cielle.
+	int array[] = { 12, 23, 34 ,45 ,56 ,67, 78, 89, 90, 101, 1212, 1234 };
 	MerkleTree* mt = NULL;
 
-	// 根据数组动态创建 Merkle Tree 
-	mt = Creat_Merkle_Tree(mt, array, sizeof(array) / sizeof(int));
+	Creat_Merkle_Tree(mt, array, sizeof(array) / sizeof(int), 0);
 
-	// 打印当前merkle Tree
-	if (mt != NULL) {
-		printf("\n开始打印当前 Merkle 树:\n");
-		Print_Merkle_Tree(mt, mt->level);
-		printf("\n\n");
-	}
 
 	return 0;
 }
-
